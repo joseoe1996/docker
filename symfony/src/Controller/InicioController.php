c706781c2326d0f702a9b28c9a4f09313e5a3336
joseoe1996 <jose.9642@hotmail.com>
2021-09-24 19:20:04 +0200

    Borrando basura de iniciocontroller

--- symfony/src/Controller/InicioController.php
+++ symfony/src/Controller/InicioController.php
@@ -11,20 +11,11 @@ class InicioController extends AbstractController {
     /**
      * @Route("/inicio", name="inicio")
      */
-<<<<<<< HEAD
     public function index(): Response
     {
         $userlog = $this->getUser()->getUsername();
         return $this->render('inicio.html.twig', [
             'controller_name' => 'Bienvenido '. $userlog,
-=======
-    public function index(): Response {
-
-        $userlog = $this->getUser();
-        
-        return $this->render('inicio.html.twig', [
-                    'controller_name' => 'InicioController',
->>>>>>> 5a8e91f5a1154c3b33e63c008af893c3d172c406
         ]);
     }
 
